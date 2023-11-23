1a462eb3d3df849c535a365ca959711c279133e8
tktk <thangkhungtheki@gmail.com>
2023-11-23 15:49:05 +0700

    add certificate https

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var app = require("./app")
+const https = require('https');
+var fs = require('fs');
+var options = {
+  key: fs.readFileSync('certificates/app.diamondplace.vn/private.key'),
+  cert: fs.readFileSync('certificates/app.diamondplace.vn/certificate.crt'),
+  ca: fs.readFileSync('certificates/app.diamondplace.vn/ca_bundle.crt')
+};
 // var http = require("http")
 
 // var server = http.createServer(app)
 
 //server.listen(4000)
 
-app.listen(3000,()=>{
-    console.log("Server run port 3000")
-})
+https.createServer(options, app).listen(3000);
+// app.listen(3000,()=>{
+//     console.log("Server run port 3000")
+// })
