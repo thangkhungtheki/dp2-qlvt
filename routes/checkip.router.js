62114a2ce759e2c481ec3e4fea4a54abd8d6f633
thangkhungtheki <thangkhungtheki@gmail.com>
2024-03-25 14:10:05 +0700

    them router check ip

--- routes/checkip.router.js
+++ routes/checkip.router.js
@@ -1,13 +1,19 @@
 var express = require("express")
 var router =  express.Router()
-
+const useragent = require('express-useragent');
+router.use(useragent.express())
 router.get('/', (req, res) => {
-    const ip = req.ip
-    const userAgent = req.get('User-Agent')
-    res.send({
-        ip: ip,
-        useragent: userAgent
-    })
+    const { isMobile, isDesktop, browser, platform, version, os } = req.useragent
+    res.render('checkinfoclient/infoclient', {
+    userAgent: req.get('User-Agent'),
+    ip:req.ip,
+    isMobile,
+    isDesktop,
+    browser,
+    platform,
+    version,
+    os
+  });
 })
 
 module.exports = router
